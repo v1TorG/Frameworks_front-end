a634015e50d9d78b9896f81f2306ccfbf7b7d905
viTor <119415099+v1TorG@users.noreply.github.com>
2025-07-28 01:19:52 -0300

    instalação do Angular e TS

--- tema 03/bloco 03/ANGULA-TS/src/app/app.module.ts
+++ tema 03/bloco 03/ANGULA-TS/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TelaboasvindasComponent } from './telaboasvindas/telaboasvindas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    
+    TelaboasvindasComponent
   ],
   imports: [
     BrowserModule,
